55819cf36db8f3b9153bad6cf975bf6eff813f4d
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-08-20 22:23:16 +0200

    [Crwn Clothing] Added header

--- crwn-clothing/src/components/collection-item/collection-item.component.jsx
+++ crwn-clothing/src/components/collection-item/collection-item.component.jsx
@@ -12,7 +12,7 @@ const CollectionItem = ({ id, name, imageUrl, price }) => (
     />
     <div className='collection-footer'>
       <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+      <span className='price'>€{price}</span>
     </div>
   </div>
 );
