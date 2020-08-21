fa540e6d374603247cd163df690ba988411b3238
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-08-21 19:39:05 +0200

    [Crwn Clothing] Implemented firebase utils, store authenticated users
    Stored authenticated users to firestore database

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -22,8 +22,21 @@ class App extends React.Component {
   componentDidMount() {
     // Subscriber listening on the current user session
     // This is a
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -13,6 +13,34 @@ const config = {
   measurementId: 'G-9HLMCK95CW',
 };
 
+// Revisit, Lecture 90 -> 94 ( to understand firebase basic auth)
+export const createUserProfileDocument = async (userAuth, additonalData) => {
+  if (!userAuth) {
+    // When user authentication exist
+    return;
+  }
+  // Now check if the user already exist
+  const userRef = firestore.doc(`users/${userAuth.uid}`); // This is for CRUD
+  const snapShot = await userRef.get(); // Snapshot reference is just for data
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additonalData,
+      });
+    } catch (error) {
+      console.error('Error creating user', error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
