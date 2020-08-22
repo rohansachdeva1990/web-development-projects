f92052ba8b44e7c45605ac09bf96776b0cc19c74
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2020-08-22 21:55:35 +0200

    [Burger Builder] Fixed CSS Module, Added Burger Builder

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/_Aux';
 
+import classes from './Layout.css';
+
 const layout = props => (
   <Aux>
     <div>ToolBar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
